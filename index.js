a14d5359d0ab3a03f2fa79bec0148dccada2ab7e
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2022-12-12 03:45:20 -0300

    update html and js files

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const { app, BrowserWindow } = require('electron')
 
 function createWindows() {
-  let appWindow = new BrowserWindow()
-  appWindow.loadURL('https://www.betteranime.net') 
+  let appWindow = new BrowserWindow({
+    width: 600,
+    height: 800,
+    center: true,
+    minWidth: 300,
+    show: false
+  })
+  appWindow.loadFile('index.html')
+
+  appWindow.once('ready-to-show', () => {
+    appWindow.show()
+  })
 }
 
 app.on('ready', createWindows)
